753330e913982e4157910411214c965941ae2349
harrylloyd-bl <harry.lloyd@bl.uk>
2024-12-02 15:10:15 +0000

    day 1 and 2

---
+++ solutions/day_1.py
@@ -0,0 +1,18 @@
+#  Clean data
+with open("..\\data\\raw\\day_1.txt", "r") as f:
+    lines = f.readlines()
+
+clean_str_list = [l.strip("\n").split("   ") for l in lines]
+list1, list2 = sorted([int(l[0]) for l in clean_str_list]), sorted([int(l[1]) for l in clean_str_list])
+
+# Pt 1
+dist = [abs(l1 - l2) for l1, l2 in zip(list1, list2)]
+total_dist = sum(dist)
+print(f"Part 1: Total distance = {total_dist}")
+
+# Pt 2
+common_locs = set(list1) & set(list2)
+similarity_score = 0
+for loc in common_locs:
+    similarity_score += list2.count(loc) * loc
+print(f"Part 2: Similarity score = {similarity_score}")
---
+++ solutions/day_2.py
@@ -0,0 +1,34 @@
+#  Clean data
+with open("..\\data\\raw\\day_2.txt", "r") as f:
+    lines = f.readlines()
+
+reports = [[int(r) for r in l.strip("\n").split(" ")] for l in lines]
+
+# Pt 1
+def classify_report(report: list, problem_damper: bool=False) -> bool:
+    if problem_damper:
+        for i, _ in enumerate(report):
+            copy = report.copy()
+            copy.pop(i)
+            if classify_report(copy, problem_damper=False):
+                return True
+        else:
+            return False
+        
+    rate_of_change = [i - j for i,j in zip(report[:-1], report[1:])]
+    mod_roc = [abs(r) for r in rate_of_change]
+
+    if min(mod_roc) < 1 or max(mod_roc) > 3:
+        return False
+    elif sum(mod_roc) != abs(sum(rate_of_change)):
+        return False
+    else:
+        return True
+    
+classified_reports = [classify_report(r) for r in reports]
+print(f"Part 1: Number of safe reports = {sum(classified_reports)}")
+
+# Pt 2
+# Modified classify_report to have a problem_handler section
+problem_damped_reports = [classify_report(r, problem_damper=True) for r in reports]
+print(f"Part 2: Number of safe reports inc problem handling = {sum(problem_damped_reports)}")
